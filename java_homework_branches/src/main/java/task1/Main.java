faa54b141bc2748c94b9fa820980f38b31e46614
IamNuke <alexeimartynyuk@gmail.com>
2023-03-16 18:42:00 +0300

    Revert "Update Main.java"
    This reverts commit 2690c007

--- java_homework_branches/src/main/java/task1/Main.java
+++ java_homework_branches/src/main/java/task1/Main.java
@@ -1,43 +1,7 @@
 package task1;
 
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        workWithLinkedList();
-    }
-
-
-    private static void workWithLinkedList() {
-        Scanner in = new Scanner(System.in);
-        LinkedList<String> wordsList = new LinkedList<>();
-
-        while (true) {
-            System.out.println();
-            System.out.print("Введите строку или команду: ");
-            String inputString = in.nextLine();
-
-            switch (inputString.toUpperCase()){
-                case "PRINT":
-                    List<String> tmp = new LinkedList<>();
-                    tmp.addAll(wordsList);
-                    Collections.reverse(tmp);
-                    System.out.println(tmp);
-                    tmp.clear();
-                    break;
-                case "REVERT":
-                    wordsList.removeLast();
-                    break;
-                case "STOP":
-                    return;
-                default:
-                    if (inputString.isBlank()) {
-                        System.out.println("Строка не должна быть пустой");
-                        continue;
-                    }
-                    wordsList.add(inputString);
-            }
-        }
+        System.out.println("Hello world!");
     }
 }
-
