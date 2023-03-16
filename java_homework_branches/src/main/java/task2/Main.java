3d82634eaccceb431400e695baaf4e722a8b582e
IamNuke <alexeimartynyuk@gmail.com>
2023-03-16 18:44:46 +0300

    Seminar 4 assignments

--- java_homework_branches/src/main/java/task2/Main.java
+++ java_homework_branches/src/main/java/task2/Main.java
@@ -1,7 +1,28 @@
 package task2;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList<String> wordsList = new LinkedList<>();
+        wordsList.add("string1");
+        wordsList.add("string2");
+        wordsList.add("string3");
+        wordsList.add("string4");
+        wordsList.add("string5");
+        System.out.println("Source list:");
+        System.out.println(wordsList);
+        LinkedList<String> revers = new LinkedList<>();
+        revers = reversList(wordsList);
+        System.out.println("Revers list:");
+        System.out.println(revers);
+    }
+
+    private static LinkedList<String> reversList(LinkedList<String> sourceList) {
+        LinkedList<String> newList = new LinkedList<>();
+        newList.addAll(sourceList);
+        Collections.reverse(newList);
+        return newList;
     }
 }
